f721ec97936256ded59b97f848a0a3a96e574136
Daniel Machin <DanielTAYG@gmail.com>
2023-08-10 18:46:32 -0400

    Worked on cleaning up the javascript, maze starting to get outline

--- js/app.js
+++ js/app.js
@@ -1,28 +1,58 @@
-// grab the startGame button
+// grabbing buttons
 const startButton = document.querySelector('#startButton')
-const startButtonfr = document.querySelector('#startButtonfr')
 const generateMaze = document.querySelector('#generateMaze')
-// Create class for players
-class Player {
-    constructor(name) {
-        this.name = name
-        this.health = 100
-        this.hitpoints = 10
-        this.speed = 5
-    }
-    gainSpeed() {
-        this.speed += 1
-    }
+
+// grab image element
+const mazeImage = document.querySelector('#mazeImage')
+
+/**
+ * This function is what generates the maze
+ */
+const createMaze = () => {
+    const maze = document.querySelector('#maze')
+    const pen = maze.getContext('2d')
+
+    pen.strokeStyle = 'black'
+    pen.lineWidth = 5
+
+    pen.beginPath();
+    pen.moveTo(100, 60);
+    pen.lineTo(950, 60);
+    pen.moveTo(80, 150);
+    pen.lineTo(80, 640);
+    pen.moveTo(80, 640);
+    pen.lineTo(1000, 640);
+    pen.moveTo(1000, 570);
+    pen.lineTo(1000, 80);
+    pen.stroke();
+    
+generateMaze.setAttribute("style","display:none;")
 }
 
 
-//grab some statsContainer divs
-const statsContainer = document.querySelector("#PlayerNames")
-const statsTitle = document.querySelector('#statsTitle')
+generateMaze.addEventListener("click", createMaze)
 
-// create blank variables
-let newPlayer
-let newPlayer2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// All of the functions I made before changing direction to do MVP goals instead, will be able to use these eventually most likely 
 
 // function to create new player's using user input
 // const startGame = () => {
@@ -42,35 +72,49 @@ let newPlayer2
 
 // }
 
+//For createMaze function
 
-const createMaze = () => {
-   for(let i = 0; i < 63; i++) {
-    const newLine = document.createElement('div')
-    newLine.classList.add('makeMaze')
-    const mazeDiv = document.querySelector('.maze-container')
-    mazeDiv.appendChild(newLine)
-}
-}
+//    for(let i = 0; i < 5; i++) {
+//     const newRow = document.createElement('div')
+//     newRow.classList.add('row')
+//     const mazeDiv = document.querySelector('.maze-container')
+//     mazeDiv.appendChild(newRow)
+//    }
+//     for(let i = 0; i < 5; i++) {
+//         const newColumn = document.createElement('div')
+//         newColumn.classList.add('column')
+//         const mazeDiv = document.querySelector('.maze-container')
+//         mazeDiv.appendChild(newColumn)
 
-const startGame = () => {
+// Create class for players
+// class Player {
+//     constructor(name) {
+//         this.name = name
+//         this.health = 100
+//         this.hitpoints = 10
+//         this.speed = 5
+//     }
+//     gainSpeed() {
+//         this.speed += 1
+//     }
+// }
 
-    const player1input = document.querySelector("#player1name")
-    const player1Name = player1input.value
-    const player2input = document.querySelector('#player2name')
-    const player2Name = player2input.value
+// const startGame = () => {
+
+//     const player1input = document.querySelector("#player1name")
+//     const player1Name = player1input.value
+//     const player2input = document.querySelector('#player2name')
+//     const player2Name = player2input.value
     
-    if(player1Name === "" || player2Name === "") {
-        alert("Please enter a valid player name")
-        return
-    }
-    newPlayer = new Player(`${player1Name}`)
-    newPlayer2 = new Player(`${player2Name}`)
+//     if(player1Name === "" || player2Name === "") {
+//         alert("Please enter a valid player name")
+//         return
+//     }
+//     newPlayer = new Player(`${player1Name}`)
+//     newPlayer2 = new Player(`${player2Name}`)
 
-    console.log(`Player 1's name is ${newPlayer.name}`)
-    console.log(`Player 2's name is ${player2Name}`)
+//     console.log(`Player 1's name is ${newPlayer.name}`)
+//     console.log(`Player 2's name is ${player2Name}`)
 
 
-}
-
-// startButtonfr.addEventListener("click", startGame)
-generateMaze.addEventListener("click", createMaze)
+// }
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # Twisted Trails
 
 # Concept of the game
-The concept of the game will be to escape the maze together by solving puzzles and working together
+The concept of the game will be to escape the maze together by solving puzzles to escape
