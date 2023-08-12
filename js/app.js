a246df902f6eadbfc7293cf3bcc34f4bbd14c6f8
Daniel Machin <DanielTAYG@gmail.com>
2023-08-12 12:14:50 -0400

    cleaned up javascript

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const startButton = document.querySelector('#startButton')
 const generateMaze = document.querySelector('#generateMaze')
 const playAgainBtn = document.querySelector('#play-again-btn')
-
+const continueBtn = document.querySelector('#continue-button')
 
   // grab image element
   const mazeImage = document.querySelector('#image')
@@ -12,6 +12,8 @@ const playAgainBtn = document.querySelector('#play-again-btn')
   const pen = maze.getContext('2d', {willReadFrequently: true })
   // grab message container element for later
   const msgContainer = document.querySelector('.message-container')
+  // grab key obtained container for later
+  const keyContainer = document.querySelector('.grabbed-key')
 
 /**
  * This function is what generates the maze
@@ -19,7 +21,7 @@ const playAgainBtn = document.querySelector('#play-again-btn')
 const createMaze = () => {
 
     // This draws the mazeImage onto the canvas
-    pen.drawImage(image, 0, 0, mazeImage.width, mazeImage.height)
+    pen.drawImage(mazeImage, 0, 0, mazeImage.width, mazeImage.height)
     
     // This gets the data from the Image to grab its Pixels
     const imageData = pen.getImageData(0, 0, mazeImage.width, mazeImage.height)
@@ -30,7 +32,7 @@ const createMaze = () => {
 
     // for loop to iterate over each pixel in the image and grab its RGBA values
     // the first loop is for each row or vertical pixels
-for (let y = 0; y < mazeImage.height; y++) {
+    for (let y = 0; y < mazeImage.height; y++) {
     // loop to go over every column for pixels
     for (let x = 0; x < mazeImage.width; x++) {
         const index = (y * mazeImage.width + x) * 4
@@ -43,8 +45,10 @@ for (let y = 0; y < mazeImage.height; y++) {
 
         // sets the "fill style" to match the value grabbed from the for loop above
         pen.fillStyle = `rgba(${r},${g},${b},${255})`
+
         // this draws a rectangle for each pixel
         pen.fillRect(x * pixelSizeX, y * pixelSizeY, pixelSizeX, pixelSizeY)
+
     }
 
     // make the generateMaze button dissapear when clicked
@@ -74,113 +78,81 @@ const key = {
     size: 32
 }
 
+// initiate keyImage for placing key on canvas
 const keyImage = new Image()
 keyImage.src = '../Images/ashWalking.png'
 
+/**
+ * this draws the key where the x and y are with its corresponding size
+ **/
 const drawKey = () => {
     pen.drawImage(keyImage,key.x, key.y, key.size, key.size)
 }
+// this draws the image once the image is fully loaded
 keyImage.onload = () => {
     drawKey()
 }
 // x: 1040, y: 150 is the winning position
-
-
-// 
 
  /**
  * rgba(241, 246, 239, 255); //white
  * rgba(0, 0, 0, 255) //black
  */
 
-//load the players image
 const playerImage = new Image()
 playerImage.src = '../Images/ashWalking.png'
 
- function drawPlayer() {
+/**
+ * This function is to draw the player
+ */
+const drawPlayer = () => {
     pen.drawImage(playerImage,player.x, player.y, player.size, player.size)
  }
+ // draw player once image is fully loaded
  playerImage.onload = () => {
     drawPlayer()
  }
-
-    // used to calculate the pixelSize compared to the canvas and the image size. Which allows the drawing to fit the canvas good
-    const pixelSizeX = Math.floor(maze.width / mazeImage.width)
-    const pixelSizeY = Math.floor(maze.height / mazeImage.height)
-
-    // This draws the mazeImage onto the canvas
-    pen.drawImage(image, 0, 0, mazeImage.width, mazeImage.height)
-    
-    // This gets the data from the Image to grab its Pixels
-    const imageData = pen.getImageData(0, 0, mazeImage.width, mazeImage.height)
-
-
-    let wallCoords = []
-    let pathCoords = []
- for (let y = 0; y < mazeImage.height; y++) {
-    for (let x = 0; x < mazeImage.width; x++) {
-        const index = (y * mazeImage.width + x) * 4
-        const r = imageData.data[index]
-        const g = imageData.data[index + 1]
-        const b = imageData.data[index + 2]
-        const a = imageData.data[index + 3]
-      
-        if (r === 0) {
-            wallCoords.push(`(${x},${y})`)
-        } else if (r === 241) {
-            pathCoords.push(`(${x},${y})`)
-        }
-
-    }
- }
-
- const checkForCollision = (playerX, playerY, playerSize) => {
-    const playerLocation = `(${player.x},${player.y})`
-
-    // if the players location is a wall coordinate then it will return true
-    if(wallCoords.includes(playerLocation)) {
-        console.log("a wall!")
-    }
-    console.log("a path!")
-    console.log(playerLocation)
- }
-
- function movePlayer(dx, dy) {
+/**
+ * This function is to move the player
+ */
+ const movePlayer = (dx, dy) => {
     const nextX = player.x + dx
     const nextY = player.y + dy
 
     // Check if the next position is within the canvas boundaries
     if (nextX >= 0 && nextX <= maze.width - player.size &&
-        nextY >= 0 && nextY <= maze.height - player.size && !checkForCollision(nextX, nextY, player.size)  ) {
+        nextY >= 0 && nextY <= maze.height - player.size) {
         player.x = nextX
         player.y = nextY
     }
  }
- console.log(wallCoords)
- console.log(pathCoords)
+ 
    // this is what moves the player when using WASD
  document.addEventListener('keydown', (e) => {
     if (e.key === 'w') {
-        movePlayer(0, -player.speed);
+        movePlayer(0, -player.speed)
     } else if (e.key === 's') {
-        movePlayer(0, player.speed);
+        movePlayer(0, player.speed)
     } else if (e.key === 'a') {
-        movePlayer(-player.speed, 0);
+        movePlayer(-player.speed, 0)
     } else if (e.key === 'd' ) {
-        movePlayer(player.speed, 0);
+        movePlayer(player.speed, 0)
     }
 
     // Clear the canvas and redraw the maze and player
     pen.clearRect(0, 0, maze.width, maze.height)
     createMaze()
-    drawPlayer()
     drawKey()
+    drawPlayer()
     console.log(`player is at x ${player.x} y ${player.y}`)
     
     if(player.x === 1040 && player.y === 150 && player.hasKey === true) {
         msgContainer.setAttribute("style", "display:block;")
     } else if (player.x === key.x && player.y === key.y) {
         player.hasKey = true
+        key.x = 1040
+        key.y = 150
+        keyContainer.setAttribute('style', 'display:block;')
         console.log(`player now hasKey is ${player.hasKey}`)
     }
 })}
@@ -188,10 +160,17 @@ playerImage.src = '../Images/ashWalking.png'
 /**
  * This function is what goes through the necessary functions to play the game
  */
-function playGame() {
+const playGame = () => {
     createMaze()
     generatePlayer()
     msgContainer.setAttribute("style", "display:none;")
+}
+
+/**
+ * This function is used to hide the grabbedKey container
+ */
+const hideMessage = () => {
+ keyContainer.setAttribute("style", "display:none;")
 }
 
 // event listener on the generate maze button to start the game
@@ -209,7 +188,7 @@ generateMaze.addEventListener("click", playGame)
  * Speaking literally
  * I basically need to check the current position of the player object and check if the R value is 0
  * If thats true then atWall will be turned to true
- * Maybe a switch Case
+ * Maybe a switch Case ?
  */
 
 
